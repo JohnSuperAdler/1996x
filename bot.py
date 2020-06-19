662e57c5b8197c191cf0fa0bed38c83799266557
Chunpin <46437534+JohnSuperAdler@users.noreply.github.com>
2020-06-19 19:30:30 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,20 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='[')
 
-@bot.event
+@bot.event #說這個叫裝飾器
 async def on_ready(): # on_ready comes from api
-    print( )
+    print('Bot is online')
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join!') #根據不同人會有不同顯示
+    channel = bot.get_channel(723496013838549104)
+    await channel.send(f'{member} join! And it was shown in channel! Wrrryyyy') # 因為send是協程(coroutine) 所以必須是呼叫 #send意味送資訊
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave!') #根據不同人會有不同
+    channel = bot.get_channel(723496013838549104)
+    await channel.send(f'{member} leave! Shiku Shiku') # 因為send是協程(coroutine) 所以必須是呼叫
+
+bot.run('NzIzMTQ5NDA5NDQxMDIyMDM1.XuySJw.CDar6TF-Ql3a3laUeCYFHmF-c8o')
